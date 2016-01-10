55803e004b3bdc7e6960a761738d65c4bec22a26
Virunee <virunee@live.co.uk>
2016-01-10 12:11:51 +0000

    Fixed messages page
    Scripted the messages page to display all messages

--- messages.php
+++ messages.php
@@ -4,28 +4,28 @@
   </head>
   <body>
     <h1>All Messages<h1>
-      <?php
+
+        <table border="0">
+        <?php
         $output = "<ul>";
         $file = fopen("entries.txt", "a+"); //opens up the txt file where the data is stored.
         $filestring = file_get_contents("entries.txt");
-        //echo "Filestring: "."$filestring<br/>";
-      $entries = strtok($filestring, ";"); //splits up the txt file string into separate entries.
-        while ($entries) {
-              echo "$entries<br/>";
-            $entries = strtok(";");
-          };
-        //$fields = explode(",", $entries);
-        //foreach ($fields as $values)
-          //echo "$values<br/>";
-      //  $fields = strtok($entries, ",");
-          //while ($fields !== false) {
-            //echo "$fields<br/>";
-            //$fields = strtok(",");
-          //$key = explode($value, ","); //splits up entries into individual answers.
-          //$output."<li><h2>$fields[0] $fields[1]</h2><p>$fields[2]</p></li>";
-        //};
-        $output."</ul>";
-        echo $output;
+      $entry = strtok($filestring, ";"); //splits by semicolon
+
+         while ($entry !== false) {
+           //echo "$entry<br/>";
+           //echo $entry;
+            $fields = explode(",", $entry);
+          //  print_r($fields);
+            $entry = strtok(";");
+            echo "<tr>".
+              "<td>Name:</td>".
+              "<td>".$fields[0]." ".$fields[1]."</td>.</tr>".
+              "<tr>".
+                "<td>Message:</td>".
+                "<td>".$fields[2]."</td></tr>";
+        };
        ?>
+       </table>
   </body>
 </html>
